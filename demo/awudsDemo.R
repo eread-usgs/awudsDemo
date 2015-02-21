5ada3a7e36f8f142d812f0d0b397da18578b4d03
David Blodgett <dblodgett@usgs.gov>
2015-02-20 18:01:22 -0600

    laid out first list of functions to implement

---
+++ demo/awudsDemo.R
@@ -0,0 +1,23 @@
+# Get Latest Data from AWUDS
+
+# Get Spatial Data from ScienceBase
+
+# Load subset of data for New Jersey
+
+# Calculate some statistics based on AWUDS
+
+# Scatter plot of county population vs a water use category.
+
+# Bar chart of water use category by county
+
+# Box and whiskers of multiple water use categories for all counties in a state.
+
+# Pie chart of water use categories for a given county
+
+# Choropleth of counties colored by water use category
+
+# Tabular data view
+
+# Map some raw and derived AWUDS data
+
+# Show how this code is being shared and can be contributed to.
